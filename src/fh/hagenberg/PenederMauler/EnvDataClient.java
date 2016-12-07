62b9bc123dac95137858189688d48359053fe049
antizwiebel <Penedermark@gmail.com>
2016-12-07 16:26:50 +0100

    almost done

--- src/fh/hagenberg/PenederMauler/EnvDataClient.java
+++ src/fh/hagenberg/PenederMauler/EnvDataClient.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Timer;
 
 /**
  * Created by Mark on 07.12.2016.
@@ -12,10 +13,11 @@ public class EnvDataClient {
 
     public static void main (String[] _argv) {
         try {
-            String adr = "//127.0.0.1:1099/EnvDataServer";
+            String adr = "EnvDataServer";
             IEnvironmentData environmentData= (IEnvironmentData) Naming.lookup(adr);
-            EnvData data= environmentData.requestEnvironmentData("luft");
-
+            //schedules a timer that runs the datatask every second
+            Timer dataTimer = new Timer();
+            dataTimer.schedule(new EnvDataTask(environmentData), 0, 1000);
         } catch (Exception _e) {
             _e.printStackTrace();
         }
--- src/fh/hagenberg/PenederMauler/EnvDataTask.java
+++ src/fh/hagenberg/PenederMauler/EnvDataTask.java
@@ -1,5 +1,6 @@
 package fh.hagenberg.PenederMauler;
 
+import java.rmi.RemoteException;
 import java.util.TimerTask;
 
 /**
@@ -7,11 +8,37 @@ import java.util.TimerTask;
  */
 public class EnvDataTask extends TimerTask {
 
-    public EnvDataTask(IEnvironmentData) {
+    private IEnvironmentData envData;
+
+    public EnvDataTask(IEnvironmentData _data) {
+        envData = _data;
     }
 
     @Override
     public void run() {
+        try {
+            String[] dataTypes=envData.requestEnvironmentDataTypes();
+            EnvData[] allEnvData;
+            System.out.println("All sensortypes: ");
+            for (String type: dataTypes) {
+                System.out.print(", "+type);
+            }
+            System.out.println();
+            allEnvData= envData.requestAll();
+            System.out.println("Requesting air pressure: ");
+            EnvData airData = envData.requestEnvironmentData("air");
+
+            System.out.println("TimeStamp"+ airData.getTimeStamp().toString()+ ", Value " + airData.getAirPressure());
+            System.out.println("Requesting all data: ");
+
+            for (EnvData e: allEnvData) {
+                System.out.print("TimeStamp"+ e.getTimeStamp().toString()+ ", Value " + e.getAirPressure());
+            }
+
+
+        } catch (Exception _e) {
+            _e.printStackTrace();
+        }
 
     }
 }
